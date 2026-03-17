0ac90768b70074a5b2b8af19294be2399093c550
Pavel547 <pawukok11@gmail.com>
2026-03-17 20:44:43 +0100

    Rename 'CLOUD_NAME' to 'CLOUDINARY_CLOUD_NAME'

--- shop/settings.py
+++ shop/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
 
 # Cloudinary storage
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUD_NAME'),
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
